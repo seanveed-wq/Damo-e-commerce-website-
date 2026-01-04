5ba9417bb5a60daaab1938fbcda375fdc6fa4778
seanveed-wq <seanveed@gmail.com>
2026-01-05 02:44:34 +0500

    Delete products.js

--- products.js
+++
@@ -1,6 +0,0 @@
-const products = [
-    { id: 1, name: "Kobe 6 Protro 'Reverse'", price: 190, img: "https://i.ibb.co/Xy7K8dK/shoe-red.png" },
-    { id: 2, name: "Kobe 4 Protro 'Philly'", price: 180, img: "https://i.ibb.co/YyYkPzY/shoe-white.png" },
-    { id: 3, name: "Kobe 8 Protro 'Venice'", price: 170, img: "https://i.ibb.co/M9M8G6r/shoe-blue.png" },
-    { id: 4, name: "Elite Mamba 'Gold'", price: 210, img: "https://i.ibb.co/hL6xHpx/shoe-white-red.png" }
-];
