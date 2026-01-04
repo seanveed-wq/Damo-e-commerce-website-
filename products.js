1df85310a0103b64a9f66d9ed9cf1716b4a677ea
seanveed-wq <seanveed@gmail.com>
2026-01-05 02:45:09 +0500

    Add initial README content for Audiobook App

---
+++ products.js
@@ -0,0 +1,6 @@
+const products = [
+    { id: 1, name: "Air Jordan 1 Retro", category: "Men's Basketball Shoe", price: 180, img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/f8941764-7935-430c-83cc-0e42d79047b8/AIR+JORDAN+1+RETRO+LOW+OG.png" },
+    { id: 2, name: "Nike Air Max Dn", category: "Big Kids' Shoes", price: 130, img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/603ec41c-b630-4e32-849c-e72252a13697/AIR+MAX+Dn.png" },
+    { id: 3, name: "Vomero 5 Premium", category: "Men's Road Running Shoes", price: 160, img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/55f410a8-b570-4226-9f79-661cc2e3b2e5/NIKE+ZOOM+VOMERO+5.png" },
+    { id: 4, name: "Nike Sportswear Tee", category: "Men's Graphic T-Shirt", price: 35, img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/d2360e2a-14d2-45e3-8551-76a0663806a6/sportswear-t-shirt-M0PzXN.png" }
+];
