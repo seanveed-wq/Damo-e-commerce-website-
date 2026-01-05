aae845f520b34cbf205356ec8707aaf159abde26
seanveed-wq <seanveed@gmail.com>
2026-01-05 21:09:30 +0500

    Delete products.js

--- products.js
+++
@@ -1,5 +0,0 @@
-const products = [
-    { id: 1, name: "Kobe 6 Protro", category: "Basketball", price: 190, img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/3752e50d-8531-4876-ba64-67252033c46e/KOBE+VI+PROTRO.png" },
-    { id: 2, name: "Air Jordan 1 Neon", category: "Lifestyle", price: 170, img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/f8941764-7935-430c-83cc-0e42d79047b8/AIR+JORDAN+1+RETRO+LOW+OG.png" },
-    { id: 3, name: "Zoom Freak 5", category: "Training", price: 140, img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/603ec41c-b630-4e32-849c-e72252a13697/AIR+MAX+Dn.png" }
-];
